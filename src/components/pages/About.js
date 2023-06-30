f1de5a7db396350a5a9100c258cbee7db4534c42
Zach Leone <zjleone@gmail.com>
2023-06-29 19:04:33 -0700

    updated about me

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -44,10 +44,14 @@ export default function About() {
       <ProfileImage src={profilePic} alt="Profile Image" />
       <AboutTitle>About Me</AboutTitle>
       <AboutText>
-        Hello, I'm Zach Leone and I'm a developer in training, although most developers are. I enjoy sailing, jetskiing, and all sorts of water sports not too available in
-        Arizona. When I'm not out exploring in my free time, I enjoy learning about new technologies and how to implement them.
-        I'm currently working on expanding my skillset in backend development using MongoDB and GraphQL. Please feel free to take the time to view
-        my projects and contact me if you have any questions.
+        Greetings! I'm Zach Leone, a passionate developer eager to learn and grow in the field. 
+        Just like many developers, I'm constantly honing my skills to stay ahead of the game.
+        Despite being located in Arizona, where water sports are limited, I find solace in sailing, 
+        jetskiing, and other aquatic activities whenever possible. When I'm not out exploring the world 
+        around me, I dive into the fascinating realm of new technologies, always curious about how they 
+        can be applied in practical ways. Currently, I'm focused on expanding my expertise in backend development, 
+        specifically with MongoDB and GraphQL. Please feel free to take the time to view
+        my projects and do not hesitate to reach out to me with any questions.
       </AboutText>
     </AboutContainer>
   );
